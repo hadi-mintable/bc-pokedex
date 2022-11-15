8cba590f18331a00ac2b871e5209f3ec92ffe61c
Hadi Lee <hadi@mintable.com>
2022-11-15 15:53:59 +0800

    Import client from file

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./ApolloClient/client";
 
 const queryClient = new QueryClient();
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "https://beta.pokeapi.co/graphql/v1beta",
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
